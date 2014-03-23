b46bb1c3df2f4143bd835021e0a600c4fb3f94e5
arjaylc <rodrigo_caljr@dlsu.ph>
2014-03-23 21:37:53 +0800

    PHP Version
    - a few minor changes

--- Bantay Bagyo/includes/header.inc.php
+++ Bantay Bagyo/includes/header.inc.php
@@ -33,25 +33,4 @@
         </div>
       </div>
     </header>
-    <div id = "mainContentWrapper">
-    <?php 
-      if($session_master->isLoggedin()){
-        include 'includes/settings.inc.php';
-      } else{
-        include 'includes/register.inc.php';
-        include 'includes/login.inc.php';
-      }
-      include 'includes/overlay.inc.php';
-    ?>
-    <script src="js/functions.js"></script>
-    <?php
-      if(isset($_GET['error'])&&$_GET['error']=='login'){?>
-      <script src="js/onload/loginerror.js"></script>
-    <?php
-      }
-      if(isset($_GET['username'])||isset($_GET['province'])||isset($_GET['email'])){?>
-      <script src="js/onload/registererror.js"></script>
-    <?php
-      }
-    ?>
     
--- Bantay Bagyo/includes/mainpage.inc.php
+++ Bantay Bagyo/includes/mainpage.inc.php
@@ -1,4 +1,23 @@
-	<?php 
+<div id = "mainContentWrapper">
+    <?php 
+      if($session_master->isLoggedin()){
+        include 'includes/settings.inc.php';
+      } else{
+        include 'includes/register.inc.php';
+        include 'includes/login.inc.php';
+      }
+      include 'includes/overlay.inc.php';
+    ?>
+    <script src="js/functions.js"></script>
+    <?php
+      if(isset($_GET['error'])&&$_GET['error']=='login'){?>
+      <script src="js/onload/loginerror.js"></script>
+    <?php
+      }
+      if(isset($_GET['username'])||isset($_GET['province'])||isset($_GET['email'])){?>
+      <script src="js/onload/registererror.js"></script>
+    <?php
+      }
       include('includes/mainpage_typhoon_details.inc.php');
       include('includes/mainpage_legend.inc.php');
       include('includes/mainpage_map.inc.php');
